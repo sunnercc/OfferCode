84800d10d956047ede260239e76ca592f477c6bd
sunner <sunner.cc@outlook.com>
2018-11-20 18:04:50 +0800

    02面试题 singleton模式

---
+++ 02面试题-实现singleton模式(c++)/02面试题-实现singleton模式(c++)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  02面试题-实现singleton模式(c++)
+//
+//  Created by ifuwo on 2018/11/20.
+//  Copyright © 2018 ifuwo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ 02面试题-实现singleton模式(oc)/02面试题-实现singleton模式(oc)/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  02面试题-实现singleton模式(oc)
+//
+//  Created by ifuwo on 2018/11/20.
+//  Copyright © 2018 ifuwo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
